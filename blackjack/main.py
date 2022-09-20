542cf917e90872e76f8e5cf8094b8250beec1486
kamilwi1994 <kamilwi1994@gmail.com>
2022-09-20 16:39:08 +0200

    Update main.py

--- blackjack/main.py
+++ blackjack/main.py
@@ -30,23 +30,23 @@ def calculate_score(cards):
 def compare(user_score, computer_score):
 
   if user_score > 21 and computer_score > 21:
-    return "You went over. You lose 😤"
+    return "You went over. You lose"
 
 
   if user_score == computer_score:
-    return "Draw 🙃"
+    return "Draw"
   elif computer_score == 0:
-    return "Lose, opponent has Blackjack 😱"
+    return "Lose, opponent has Blackjack"
   elif user_score == 0:
-    return "Win with a Blackjack 😎"
+    return "Win with a Blackjack"
   elif user_score > 21:
-    return "You went over. You lose 😭"
+    return "You went over. You lose"
   elif computer_score > 21:
-    return "Opponent went over. You win 😁"
+    return "Opponent went over. You win"
   elif user_score > computer_score:
-    return "You win 😃"
+    return "You win"
   else:
-    return "You lose 😤"
+    return "You lose"
 
 def play_game():
 
@@ -86,7 +86,7 @@ def play_game():
   print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
   print(compare(user_score, computer_score))
 
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
   clear()
   play_game()
